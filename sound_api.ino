00c50b60e4b5e29dfb6c302c54d997f765b8b04f
shou-kato <katou-s@cymes.co.jp>
2021-09-20 22:10:35 +0900

    wifi接続初期設定

--- sound_api.ino
+++ sound_api.ino
@@ -1,9 +1,30 @@
 #include <WiFi.h>
 
+  // TODO 環境変数の使い方を調べる
+  const char SSID[] = "ssid"
+  const char PASSWORD[] = "wifi paddword"
+  WiFiServer server(80);
+
 void setup()
 { 
-  
-    Serial.begin(115200);
+  Serial.begin(115200);
+  while (!Serial);
+
+  WiFi.begin(SSID, PASSWORD);
+  Serial.print("WiFi connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println(" connected");
+
+  server.begin();
+
+  Serial.print("HTTP Server: http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
 }
  
 void loop()
